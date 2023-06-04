e687f22a1625dd899bb6954c8c8e467dbf8c5f61
itsWindows11 <itswin11@outlook.com>
2023-06-04 20:08:55 +0300

    Removed redundant `JsonPropertyName`s

--- Models/DailyForecast.cs
+++ Models/DailyForecast.cs
@@ -9,34 +9,24 @@ namespace weatherAPI.Models
 {
     public sealed class DailyForecast
     {
-        [JsonPropertyName("time")]
         public string Time { get; set; }
 
-        [JsonPropertyName("temperatureMax")]
         public double TemperatureMax { get; set; }
 
-        [JsonPropertyName("temperatureMin")]
         public double TemperatureMin { get; set; }
 
-        [JsonPropertyName("weatherCode")]
         public int WeatherCode { get; set; }
 
-        [JsonPropertyName("apparentTemperatureMax")]
         public double ApparentTemperatureMax { get; set; }
 
-        [JsonPropertyName("apparentTemperatureMin")]
         public double ApparentTemperatureMin { get; set; }
 
-        [JsonPropertyName("sunrise")]
         public string Sunrise { get; set; }
 
-        [JsonPropertyName("sunset")]
         public string Sunset { get; set; }
 
-        [JsonPropertyName("windSpeedMax")]
         public double WindSpeedMax { get; set; }
 
-        [JsonPropertyName("windDirectionDominant")]
         public int WindDirectionDominant { get; set; }
     }
 }
--- Models/HourlyForecast.cs
+++ Models/HourlyForecast.cs
@@ -4,19 +4,14 @@ namespace weatherAPI.Models
 {
     public sealed class HourlyForecast
     {
-        [JsonPropertyName("time")]
         public string Time { get; set; }
 
-        [JsonPropertyName("temperature")]
         public double Temperature { get; set; }
 
-        [JsonPropertyName("weatherCode")]
         public int WeatherCode { get; set; }
 
-        [JsonPropertyName("windSpeed")]
         public double WindSpeed { get; set; }
 
-        [JsonPropertyName("windDirection")]
         public int WindDirection { get; set; }
     }
 }
